4fa7d56ee3237fe3f8eb5c21d57b060cd5480e9a
Sebatian <Sxe1exs@gmail.com>
2023-12-08 10:59:08 -0700

    Added a favorite state to MovieItems

--- src/componants/MovieList/MovieList.jsx
+++ src/componants/MovieList/MovieList.jsx
@@ -2,13 +2,13 @@ import MovieItem from "../MovieItem/MovieItem";
 
 const MovieList = ({}) => {
   const movies = ["Princess Bride", "Iron Man", "Pulp Fiction", "Enders Game"];
-  const movieItems = movies.map((movie) => (
+  const MovieItems = movies.map((movie) => (
     <MovieItem key={movie} title={movie} />
   ));
   return (
     <div className="flex-item">
       <h4>My Movies</h4>
-      <div className="infoText">{movieItems}</div>
+      <div className="infoText">{MovieItems}</div>
     </div>
   );
 };

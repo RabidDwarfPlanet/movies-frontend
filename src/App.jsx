0e4d388dfca9930e3fef880045a8d1e793c8451e
Sebatian <Sxe1exs@gmail.com>
2023-12-08 12:06:42 -0700

    MovieForm Added

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,25 @@ import "./App.css";
 import Header from "./componants/Header/Header.jsx";
 import MovieList from "./componants/MovieList/MovieList.jsx";
 import MovieInfo from "./componants/MovieInfo/MovieInfo.jsx";
+import NewMovieForm from "./componants/NewMovieForm/NewMovieForm.jsx";
 
 function App() {
   const selectedMovie = {
-    title:'Princess Bride',
-    runningTime:13, 
-    genre:'Romantic Genre'
+    title: "Princess Bride",
+    runningTime: 13,
+    genre: "Romantic Genre",
   };
 
-  return <div className="App">
-    <Header/>
-    <div className="flex-container">
-      <MovieList/>
-      <MovieInfo movieObj={selectedMovie}/>
+  return (
+    <div className="App">
+      <Header />
+      <div className="flex-container">
+        <MovieList />
+        <MovieInfo movieObj={selectedMovie} />
+        <NewMovieForm />
+      </div>
     </div>
-    
-  </div>
+  );
 }
 
 export default App;
